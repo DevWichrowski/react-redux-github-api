cf2b0b7ae20a28b48cd126aef48c95383bb1e9f1
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-19 20:00:56 +0100

    Added  components header footer etc.

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        <Header />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Main;
