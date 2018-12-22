b1f7aa4f4cff08d3b39ee901d5cd8f08d1d8e688
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-22 14:04:19 +0100

    Add state and button to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './Header.scss';
 import GithubLogo from '../../assets/github-white.png';
 import { Button } from 'react-bootstrap';
 
-export default function Header() {
-  return (
-    <div className="Header">
-       <a href="https://github.com/"><img className="logo" src={GithubLogo} /></a>
-      <input className="search-input"/>
-      <Button bsStyle="primary">Search</Button>
-    </div>
-  )
+export default class Header extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        searchInput: ''
+    }
+  }
+
+  saveUserInState = (e) =>{
+    this.setState({searchInput: e.target.value})
+    console.log(this.state.searchInput)
+  } 
+
+  render() {
+    return (
+      <div className="Header">
+      <a href="https://github.com/"><img className="logo" src={GithubLogo} /></a>
+     <input className="search-input" onChange={this.saveUserInState}/>
+     <Button bsStyle="primary">Search</Button>
+   </div>
+    )
+  }
 }
