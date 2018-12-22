9a858f641b94a757d2a943becfd547015838072f
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-22 13:56:18 +0100

    Added react bootstrap and input with buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Header.scss';
 import GithubLogo from '../../assets/github-white.png';
+import { Button } from 'react-bootstrap';
 
 export default function Header() {
   return (
     <div className="Header">
        <a href="https://github.com/"><img className="logo" src={GithubLogo} /></a>
+      <input className="search-input"/>
+      <Button bsStyle="primary">Search</Button>
     </div>
   )
 }
