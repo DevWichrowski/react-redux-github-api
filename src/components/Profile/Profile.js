fb06c29a2d2b203c446145cce8ba35afa7333deb
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-23 21:09:27 +0100

    Add popover but working on it.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Image } from 'react-bootstrap';
 import { Glyphicon } from 'react-bootstrap';
+import { Popover } from 'react-bootstrap';
 import './Profile.scss';
 
 class Profile extends Component {
@@ -14,7 +15,27 @@ class Profile extends Component {
 		}
 
 		if (loading) {
-			return <div> <h2>Loading... </h2></div>;
+			return (
+				<div>
+					{' '}
+					<h2>Loading... </h2>
+				</div>
+			);
+		}
+
+		const showPersonalInfo = () =>{
+			return (
+			<div style={{ height: 120 }}>
+				<Popover
+				  id="popover-basic"
+				  placement="right"
+				  positionLeft={200}
+				  positionTop={50}
+				  title="Popover right"
+				>
+				  And here's some <strong>amazing</strong> content. It's very engaging. right?
+				</Popover>
+			  </div>);
 		}
 
 		return (
@@ -22,15 +43,27 @@ class Profile extends Component {
 				{this.props.gitstats.personalInfo.login ? (
 					<div className="personal-data">
 						<div>
-							<Image src={`${this.props.gitstats.personalInfo.avatar_url}`} className="profil-image" circle />
-							<p className="username"><strong>{this.props.gitstats.personalInfo.login}</strong></p>
-								{/* <p>{this.props.gitstats.personalInfo.name}</p>
+							<Image
+								src={`${this.props.gitstats.personalInfo.avatar_url}`}
+								className="profil-image"
+								onMouseEnter={() => showPersonalInfo()}
+								onMouseLeave={() => console.log('leave')}
+								circle
+							/>
+							<p className="username">
+								<strong>{this.props.gitstats.personalInfo.login}</strong>
+							</p>
+							{/* <p>{this.props.gitstats.personalInfo.name}</p>
 							<p>{this.props.gitstats.personalInfo.company}</p>
 							<p>{this.props.gitstats.personalInfo.location}</p> */}
 						</div>
 						<div>
-							<p><Glyphicon glyph="user" />Followers {this.props.gitstats.personalInfo.followers}</p>
-							<p><Glyphicon glyph="user" />Following {this.props.gitstats.personalInfo.following}</p>
+							<p>
+								<Glyphicon glyph="user" />Followers {this.props.gitstats.personalInfo.followers}
+							</p>
+							<p>
+								<Glyphicon glyph="user" />Following {this.props.gitstats.personalInfo.following}
+							</p>
 						</div>
 					</div>
 				) : (
