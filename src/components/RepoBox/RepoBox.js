27e4c8810acd19b3960351418aa10a0fdbfee578
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-28 22:43:28 +0100

    Add new component for single boxed repo

---
+++ src/components/RepoBox/RepoBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './RepoBox.scss';
+
+class RepoBox extends Component {
+  render() {
+    return (
+      <div>
+        <p>repobox</p>
+      </div>
+    )
+  }
+}
+
+export default RepoBox;
