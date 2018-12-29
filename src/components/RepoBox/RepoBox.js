23b1abf2d0a32d3f6a71320e7b57dd67d59218f7
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-12-29 20:25:12 +0100

    Refactor

--- src/components/RepoBox/RepoBox.js
+++ src/components/RepoBox/RepoBox.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
 import './RepoBox.scss';
-import { fetchReposData } from '../../store/actions/reposInfoAction';
-import { connect } from 'react-redux';
 
 class RepoBox extends Component {
-	componentDidMount() {
-		this.props.dispatch(fetchReposData('devwichrowski'));
-	}
 
 	render() {
 		return (
 			<div className="RepoBox">
-				{this.props.reposInfo.reposInfoArr.map((repo, index) =>{
-					return(
-					<div key={index}>
-						<h3>{repo.name}</h3>
-					</div>);
-				})}
+				<p>asd</p>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = (state) => ({
-	reposInfo: state.reposInfo
-});
-
-export default connect(mapStateToProps, null)(RepoBox);
+export default RepoBox;
--- src/components/ReposContainer/ReposContainer.js
+++ src/components/ReposContainer/ReposContainer.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './ReposContainer.scss';
 import RepoBox from '../RepoBox/RepoBox';
+import { fetchReposData } from '../../store/actions/reposInfoAction';
+import { connect } from 'react-redux';
+
 
 class ReposContainer extends Component {
+	componentDidMount() {
+		this.props.dispatch(fetchReposData('devwichrowski'));
+	}
+
 	render() {
 		return (
 			<div className="ReposContainer">
-				<p>repo container</p>
-				<RepoBox />
+				{this.props.reposInfo.reposInfoArr.map((repo, index) =>{
+					return(
+					<div key={index}>
+						<RepoBox />
+					</div>);
+				})}
 			</div>
 		);
 	}
 }
 
-export default ReposContainer;
+const mapStateToProps = (state) => ({
+	reposInfo: state.reposInfo
+});
+
+export default connect(mapStateToProps, null)(ReposContainer);
