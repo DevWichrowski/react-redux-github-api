0175f48672ccdfe78d7c752b2b815f7370dd8a56
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-06 19:04:45 +0100

    Add boxshadow

--- src/components/RepoBox/RepoBox.js
+++ src/components/RepoBox/RepoBox.js
@@ -5,14 +5,24 @@ class RepoBox extends Component {
 	constructor(props) {
 		super(props);
 	}
-
 	render() {
+		const currentdate = new Date(); 
+	const datetime = "Last Sync: " + currentdate.getDate() + "/"
+					+ (currentdate.getMonth()+1)  + "/" 
+					+ currentdate.getFullYear() + " @ "  
+					+ currentdate.getHours() + ":"  
+					+ currentdate.getMinutes() + ":" 
+					+ currentdate.getSeconds();
 		return (
 			<div className="RepoBox">
 				<h3>{this.props.name}</h3>
 				<div className='dived-badge'>{this.props.devLang}</div>
 					{this.props.description !== null ? <p>{this.props.description}</p> : <p> no description :(</p> }
-				
+				<div className="RepoBox-Button">
+				<p>{datetime}</p>
+				{/* <div className="dived-badge">Clone</div>
+				<div className="dived-badge">Visit</div> */}
+				</div>
 			</div>
 		);
 	}
