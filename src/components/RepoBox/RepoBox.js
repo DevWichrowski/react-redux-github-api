5f4527e02130fa16d2f2a8fafca3969690c09af6
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-07 22:14:48 +0100

    Add momentjs to project

--- src/components/RepoBox/RepoBox.js
+++ src/components/RepoBox/RepoBox.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './RepoBox.scss';
+import moment from 'moment';
 
 class RepoBox extends Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
+		console.log('moment ' + moment.now());
 		return (
 			<div className="RepoBox">
 				<h3>{this.props.name}</h3>
