dc5756cf2e80dd3f850a03f946b3eb92023d07f4
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-07 22:44:06 +0100

    Sorted repos from api

--- src/components/ReposContainer/ReposContainer.js
+++ src/components/ReposContainer/ReposContainer.js
@@ -12,9 +12,20 @@ class ReposContainer extends Component {
 	}
 
 	render() {
+		const reposFromApi = this.props.reposInfo.reposInfoArr;
+
+		const sortRepos = (repo) =>{
+			repo.sort(function compare(a, b){
+				const dateOne = new Date(a.updated_at);
+				const dateTwo = new Date(b.updated_at);
+				return dateTwo - dateOne;
+			})	
+		}
+	
 		return (
 			<div className="ReposContainer">
-				{this.props.reposInfo.reposInfoArr.map((repo, index) =>{
+				{sortRepos(reposFromApi)}
+				{reposFromApi.map((repo, index) =>{
 					return(
 					<div key={index}>
 						<RepoBox name={repo.name} description={repo.description} devLang={repo.language} lastUpdate={repo.updated_at}/>
