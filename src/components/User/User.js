f11cc2abcce0903476d2213a9a1bf847d968e932
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-12 18:04:16 +0100

    Add button to user component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import { Glyphicon } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './User.scss';
 
 export default function User(props) {
@@ -11,7 +12,7 @@ export default function User(props) {
 			</div>
 			<div className="infos-right">
 				{props.login !== null ? <p>{props.login}</p> : null}
-                {props.profile !== null ? <p>{props.profile}</p> : null}
+                {props.profile !== null ? <a href={props.profile}><Button bsStyle="primary">Go to profile</Button></a> : null}
 			</div>
 		</div>
 	);
