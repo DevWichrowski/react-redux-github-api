336c7587fa791020b2a0f5e3427d4d1bae783046
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-12 14:43:06 +0100

    Add authToken for extended API limits

--- src/store/sagas/gerFollowersInfoApi.js
+++ src/store/sagas/gerFollowersInfoApi.js
@@ -1,13 +1,14 @@
 import { put } from 'redux-saga/effects';
+import { authToken } from '../../authToken';
 
-export default function* getFollowersInfoApi(action){
-    try {
-		const payload = yield fetch(`https://api.github.com/users/${action.payload}/followers`, {
-			method: 'get',
+export default function* getFollowersInfoApi(action) {
+	try {
+		const payload = yield fetch(`https://api.github.com/users/${action.payload}/followers${authToken}`, {
+			method: 'get'
 		}).then((response) => response.json());
 
 		yield put({ type: '[followersInfoAction] - SUCCESS_POSTS', payload });
 	} catch (err) {
-		console.log(err)
+		console.log(err);
 	}
 }
--- src/store/sagas/getReposApi.js
+++ src/store/sagas/getReposApi.js
@@ -1,16 +1,16 @@
 import { put } from 'redux-saga/effects';
+import { authToken } from '../../authToken';
 
 // Funkcja, tak zwany generator. W ponizszym przykladzie uderzam w usera, poczym wywolujemy akcje o typie [personalInfoAction] - SUCCESS_POSTS z payloadem, ktorzy otrzymalismy spowrotem z api.
 export default function* getReposApi(action) {
 	console.log(action.payload);
 	try {
-		const payload = yield fetch(`https://api.github.com/users/${action.payload}/repos`, {
-			method: 'get',
+		const payload = yield fetch(`https://api.github.com/users/${action.payload}/repos${authToken}`, {
+			method: 'get'
 		}).then((response) => response.json());
 
 		yield put({ type: '[reposInfoAction] - SUCCESS_POSTS', payload });
 	} catch (err) {
-		console.log(err)
+		console.log(err);
 	}
 }
-
