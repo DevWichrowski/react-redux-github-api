a2d802957d639de59f663334d10fd8dedf364afd
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-13 19:06:16 +0100

    Add more styles for Gists and User component.

--- src/components/Gist/Gist.js
+++ src/components/Gist/Gist.js
@@ -4,7 +4,8 @@ import './Gist.scss';
 const Gist = (props) => {
     return (
         <div className='Gist'>
-            <h4>{props.name}</h4>
+            <h4><a href={props.htmlUrl}>{props.name}</a></h4>
+            <p>{props.description}</p>
         </div>
     );
 }
