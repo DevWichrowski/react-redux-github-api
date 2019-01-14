a7053f7c6f227bc8cfaa534108a90e301daaf36c
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-14 20:15:21 +0100

    Add saveRepoName function in repoActions

--- src/store/actions/reposInfoAction.js
+++ src/store/actions/reposInfoAction.js
@@ -4,6 +4,7 @@ export const FETCH_DETAILED_REPOS = 'FETCH_DETAILED_REPOS';
 export const FETCH_FAILURE = '[reposInfoAction] - FAIL';
 export const GET_REPOS = 'GET_REPOS';
 export const GET_DETAILED_REPO = 'GET_DETAILED_REPO';
+export const SAVE_REPO_NAME = 'SAVE_REPO_NAME';
 
 export const fetchBegin = () => ({ type: FETCH_REQUEST });
 export const fetchRepos = (payload) => ({ type: FETCH_REPOS, payload });
@@ -11,6 +12,7 @@ export const fetchDetailedRepos = (payload) => ({ type: FETCH_DETAILED_REPOS, pa
 export const fetchFailure = (payload) => ({ type: FETCH_FAILURE, payload });
 export const getRepos = (payload) => ({ type: GET_REPOS, payload });
 export const getDetailedRepo = (payload) => ({ type: GET_DETAILED_REPO, payload });
+export const saveRepoName = (payload) => ({ type: SAVE_REPO_NAME, payload });
 
 // Handle HTTP errors since fetch won't.
 // const handleErrors = (response) => {
