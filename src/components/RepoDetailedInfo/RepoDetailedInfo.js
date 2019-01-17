4c773d3e5052e7f336b6e2a243ccae396cb2a980
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-17 16:26:12 +0100

    Placed data from api in divs and implemented time with moment lib.

--- src/components/RepoDetailedInfo/RepoDetailedInfo.js
+++ src/components/RepoDetailedInfo/RepoDetailedInfo.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import {connect} from "react-redux";
 import './RepoDetailedInfo.scss';
+import {Button} from "react-bootstrap";
+import moment from "moment";
 
 function RepoDetailedInfo(props) {
+    const createdAt = moment(props.reposInfo.created_at).fromNow();
+    const lastUpdate = moment(props.reposInfo.updated_at).fromNow();
+
     return (
         <div className="RepoDetailedInfo">
             {props.reposInfo.name !== undefined ? (
                 <div className="detailed-repo">
                     <h2>{props.reposInfo.name}</h2>
                     <hr size="30"/>
-                    <p>go visist {props.reposInfo.html_url}</p>
+                    <div className="repo-dates">
+                        <p>Created at: {createdAt}</p>
+                        <p>Last update: {lastUpdate}</p>
+                    </div>
+                    <div className="repo-lang">
+                        <p>Language: {props.reposInfo.language}</p>
+                        <p>default branch {props.reposInfo.default_branch}</p>
+                        <p>subscribers: {props.reposInfo.subscribers_count}</p>
+                    </div>
+
                     <p>description: {props.reposInfo.description}</p>
-                    <p>default branch {props.reposInfo.default_branch}</p>
-                    <p>git url {props.reposInfo.git_url}</p>
-                    <p>ssh url {props.reposInfo.ssh_url}</p>
-                    <p>clone url {props.reposInfo.clone_url}</p>
-                    <p>svn url {props.reposInfo.svn_url}</p>
-                    <p>subscribers: {props.reposInfo.subscribers_count}</p>
-                    <p>created at: {props.reposInfo.created_at}</p>
-                    <p>Last update: {props.reposInfo.updated_at}</p>
-                    <p>Language: {props.reposInfo.language}</p>
+
+                    <div clasName="github-buttons">
+                        <p>git url {props.reposInfo.git_url}</p>
+                        <p>ssh url {props.reposInfo.ssh_url}</p>
+                        <p>clone url {props.reposInfo.clone_url}</p>
+                        <p>svn url {props.reposInfo.svn_url}</p>
+                    </div>
+                    <div className="detailed-navs-button">
+                        <Button bsStyle="success"><a href={props.reposInfo.html_url}>Visit on Github</a></Button>
+                        <Button bsStyle="primary"> Go back </Button>
+                    </div>
                 </div>
             ) : <h1>Loading...</h1>}
         </div>
