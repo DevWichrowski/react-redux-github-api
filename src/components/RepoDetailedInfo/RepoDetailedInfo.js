84414054f50a9c1bb06210257b7d340907434aec
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-17 14:54:29 +0100

    Add hr and connected RepoDetailedInfo to store.

--- src/components/RepoDetailedInfo/RepoDetailedInfo.js
+++ src/components/RepoDetailedInfo/RepoDetailedInfo.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {connect} from "react-redux";
+import './RepoDetailedInfo.scss';
 
 function RepoDetailedInfo(props) {
     return (
-        <div>
-            <h1>Repo detailed</h1>
+        <div className="RepoDetailedInfo">
+            <h1>Detailed Repo information</h1>
+            <hr size="30"/>
+            <div className="detailed-repo">
+            <h2>{props.reposInfo.name}</h2>
+            </div>
         </div>
     );
 }
+const mapStateToProps = (state) => ({
+    reposInfo: state.reposInfo.repoDetailedArr,
+});
 
-export default RepoDetailedInfo;
+export default connect(mapStateToProps, null)(RepoDetailedInfo);
