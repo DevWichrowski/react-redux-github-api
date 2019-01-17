2cf3ca73cdf6bdaacf5779c68cafe27d6facec55
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-17 15:17:04 +0100

    Add repo detailed info from store.

--- src/components/RepoDetailedInfo/RepoDetailedInfo.js
+++ src/components/RepoDetailedInfo/RepoDetailedInfo.js
@@ -5,14 +5,21 @@ import './RepoDetailedInfo.scss';
 function RepoDetailedInfo(props) {
     return (
         <div className="RepoDetailedInfo">
-            <h1>Detailed Repo information</h1>
-            <hr size="30"/>
             <div className="detailed-repo">
-            <h2>{props.reposInfo.name}</h2>
+                <h2>{props.reposInfo.name}</h2>
+                <hr size="30"/>
+                <p>go visist {props.reposInfo.html_url}</p>
+                <p>description: {props.reposInfo.description}</p>
+                <p>default branch {props.reposInfo.default_branch}</p>
+                <p>git url {props.reposInfo.git_url}</p>
+                <p>ssh url {props.reposInfo.ssh_url}</p>
+                <p>clone url {props.reposInfo.clone_url}</p>
+                <p>svn url {props.reposInfo.svn_url}</p>
             </div>
         </div>
     );
 }
+
 const mapStateToProps = (state) => ({
     reposInfo: state.reposInfo.repoDetailedArr,
 });
