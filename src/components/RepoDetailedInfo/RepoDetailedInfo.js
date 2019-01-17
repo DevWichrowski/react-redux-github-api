4a541d91e8f93bd1200f87cab632de0ea642eded
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-17 17:44:52 +0100

    added copy to clipboard to repo urls, styled repo container

--- src/components/RepoDetailedInfo/RepoDetailedInfo.js
+++ src/components/RepoDetailedInfo/RepoDetailedInfo.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {connect} from "react-redux";
 import './RepoDetailedInfo.scss';
-import {Button} from "react-bootstrap";
+import {Button, Glyphicon, Tooltip, OverlayTrigger} from "react-bootstrap";
 import moment from "moment";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 function RepoDetailedInfo(props) {
     const createdAt = moment(props.reposInfo.created_at).fromNow();
     const lastUpdate = moment(props.reposInfo.updated_at).fromNow();
+
+    const tooltip = (
+        <Tooltip id="tooltip">
+            <strong>Copy to clipboard!</strong>
+        </Tooltip>
+    );
 
     return (
         <div className="RepoDetailedInfo">
@@ -20,8 +27,18 @@ function RepoDetailedInfo(props) {
                             Last update: {lastUpdate}
                         </div>
                     </div>
-                    <div className="repo-name">
-                        {props.reposInfo.name}
+                    <div class="main-info">
+                        <div className="repo-name">
+                            {props.reposInfo.name}
+                        </div>
+                        <div className="repo-desc">
+                            <div className="desc-header">
+                                Description:
+                            </div>
+                            <div className="desc-body">
+                                {props.reposInfo.description}
+                            </div>
+                        </div>
                     </div>
                     <div className="repo-detailed-info">
                         <div className="info">
@@ -34,27 +51,52 @@ function RepoDetailedInfo(props) {
                             Subscribers: {props.reposInfo.subscribers_count}
                         </div>
                     </div>
-                    <div className="repo-desc">
-                        <div className="desc-header">
-                            Description:
-                        </div>
-                        <div className="desc-body">
-                            {props.reposInfo.description}
-                        </div>
-                    </div>
                     <div className="repo-actions">
-                        <div className="action">
-                            Git url {props.reposInfo.git_url}
-                        </div>
-                        <div className="action">
-                            Ssh url {props.reposInfo.ssh_url}
-                        </div>
-                        <div className="action">
-                            Clone url {props.reposInfo.clone_url}
-                        </div>
-                        <div className="action">
-                            svn url {props.reposInfo.svn_url}
-                        </div>
+                        <OverlayTrigger placement="top" overlay={tooltip}>
+                            <CopyToClipboard text={props.reposInfo.git_url}>
+                                <div className="action">
+                                    <div>
+                                        GIT URL: {props.reposInfo.git_url}
+                                    </div>
+                                    <Glyphicon className="stats-icons"
+                                               glyph="copy"/>
+                                </div>
+                            </CopyToClipboard>
+                        </OverlayTrigger>
+                        <OverlayTrigger placement="top" overlay={tooltip}>
+                            <CopyToClipboard text={props.reposInfo.ssh_url}>
+                                <div className="action">
+                                    <div>
+                                        SSH URL: {props.reposInfo.ssh_url}
+                                    </div>
+                                    <Glyphicon className="stats-icons"
+                                               glyph="copy"/>
+                                </div>
+                            </CopyToClipboard>
+                        </OverlayTrigger>
+                        <OverlayTrigger placement="top" overlay={tooltip}>
+                            <CopyToClipboard text={props.reposInfo.clone_url}>
+                                <div className="action">
+                                    <div>
+                                        CLONE URL: {props.reposInfo.clone_url}
+                                    </div>
+                                    <Glyphicon className="stats-icons"
+                                               glyph="copy"/>
+                                </div>
+                            </CopyToClipboard>
+                        </OverlayTrigger>
+                        <OverlayTrigger placement="top" overlay={tooltip}>
+                            <CopyToClipboard text={props.reposInfo.svn_url}>
+                                <div className="action">
+                                    <div>
+                                        SVN URL: {props.reposInfo.svn_url}
+                                    </div>
+
+                                    <Glyphicon className="stats-icons"
+                                               glyph="copy"/>
+                                </div>
+                            </CopyToClipboard>
+                        </OverlayTrigger>
                     </div>
                 </div>
             ) : <h1>Loading...</h1>}
